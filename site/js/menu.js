6cbe71a4325b94a32f0617d66f14cb81dfddc34d
itggot-josef-ngo <josef.ngo@itggot.se>
2018-03-29 19:32:46 +0200

    forts

--- site/js/menu.js
+++ site/js/menu.js
@@ -2,9 +2,12 @@ function toggleMenu(){
     var element = document.querySelector(".menu");
     element.classList.toggle("show")
 
-    element = document.querySelector(".menu-container");
-    element.classList.toggle("hide")
+   	element = document.querySelector(".faq");
+    element.classList.toggle("display")
 
-    element = document.querySelector(".close")
-    element.classList.toggle("show")
+    element = document.querySelector(".contact");
+    element.classList.toggle("display")
+
+    element = document.querySelector(".resa");
+    element.classList.toggle("display")
 }
